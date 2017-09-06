13579650ebaba78aab4c09aadc2ed4e69e3f3373
DHL <dhl1402@gmail.com>
2017-09-06 10:18:17 +0700

    Init

---
+++ src/App.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+} from 'react-native';
+
+export default class App extends Component {
+
+    state = {
+        selectedBar: null
+    }
+
+    onPressHandler = (index) => {
+        this.setState({
+            selectedBar: index
+        });
+    }
+
+    render() {
+        const {
+            container,
+            buttonWrapper,
+            button,
+            text,
+            barWrapper,
+            bar
+        } = styles;
+
+        const { selectedBar } = this.state;
+
+        return (
+            <View style={container}>
+                <View style={buttonWrapper}>
+                    <TouchableOpacity
+                        style={button}
+                        onPress={() => this.onPressHandler(1)}
+                    >
+                        <Text style={text}>1</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={button}
+                        onPress={() => this.onPressHandler(2)}
+                    >
+                        <Text style={text}>2</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={button}
+                        onPress={() => this.onPressHandler(3)}
+                    >
+                        <Text style={text}>3</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={barWrapper}>
+                    <View
+                        style={[
+                            bar,
+                            {
+                                width: selectedBar === 1 ? '100%' : `${Math.floor((Math.random() * 50) + 1)}%`
+                            }
+                        ]}
+                    />
+                    <View
+                        style={[
+                            bar,
+                            {
+                                width: selectedBar === 2 ? '100%' : `${Math.floor((Math.random() * 50) + 1)}%`
+                            }
+                        ]}
+                    />
+                    <View
+                        style={[
+                            bar,
+                            {
+                                width: selectedBar === 3 ? '100%' : `${Math.floor((Math.random() * 50) + 1)}%`
+                            }
+                        ]}
+                    />
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 30
+    },
+
+    buttonWrapper: {
+        flexDirection: 'row',
+        height: 100,
+        backgroundColor: '#1F1F1F'
+    },
+
+    button: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 0.5,
+        borderColor: '#d6d7da',
+    },
+
+    text: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+
+    barWrapper: {
+        marginTop: 20
+    },
+
+    bar: {
+        height: 35,
+        marginBottom: 5,
+        backgroundColor: 'red',
+    }
+})
